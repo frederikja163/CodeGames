b0c4bec6f1333a3b1e3c33e978022014e293dab8
Oliver Engbjerg Christensen <oliverec04@gmail.com>
2020-01-16 18:33:28 +0100

    button text

--- Client/main.js
+++ Client/main.js
@@ -1,36 +1,15 @@
 ///<reference path="p5.global-mode.d.ts" />
-///<reference path="oliverButton.js" />
 
 var velX = 10;
 var velY = 25;
 var x = 10;
 var y = 20;
 
-
-//Jeg kunne ikke lave objekt fra anden fil, så tester her
-class Button
-{
-    constructor(pos, size, text, colour, onHover, onClick)
-    {
-        this.posX = pos[0]
-        this.posY = pos[1]
-        this.sizeX = size[0]
-        this.sizeY = size[1]
-        this.text = text
-        this.colour = colour
-    }
-
-    draw()
-    {
-        rect(this.posX, this.posY, this.sizeX, this.sizeY)
-    }
-}
-
 function setup()
 {
     createCanvas(1069, 420);
 
-    button1 = new Button([100, 100], [269, 80], "test", [100, 100, 100], 0, 0);
+    button1 = new Button([100, 100], [269, 80], "test hello world", [169, 50, 100], 0, 0);
 }
 
 function draw()
@@ -50,10 +29,6 @@ function draw()
     }
 
     rect(x, y, 50, 50);
-
-
-
-
 
     button1.draw();
 }
--- Client/oliverButton.js
+++ Client/oliverButton.js
@@ -9,11 +9,29 @@ class Button
         this.sizeX = size[0]
         this.sizeY = size[1]
         this.text = text
+        this.textColour = [0, 0, 100]
+        this.textSize = this.sizeY / 4
         this.colour = colour
+    }
+
+    setup()
+    {
+        if (this.colour[2] >= 50)
+        {
+            this.textColour = [0, 0, 0]
+        }
     }
 
     draw()
     {
+        colorMode(HSB);
+
+        fill(this.colour)
         rect(this.posX, this.posY, this.sizeX, this.sizeY)
+        
+        fill(this.textColour)
+        textSize(this.textSize)
+        text(this.text, this.posX + this.sizeX / 2 - textWidth(this.text) / 2, this.posY + this.sizeY / 2 - this.textSize / 2, this.sizeX, this.sizeY)
+
     }
 }
