4258f0bed946c1e4d9efa67bfed75c34f774b4c6
Oliver Engbjerg Christensen <oliverec04@gmail.com>
2020-01-26 17:40:20 +0100

    menu button

--- Client/Lobby/menu.js
+++ Client/Lobby/menu.js
@@ -2,16 +2,37 @@ class Menu
 {
     constructor()
     {
+        this.buttonColor = color(.7, 1, .5);
+        this.buttonSize = createVector(height * .2, height * .1);
+        this.startButton = new Button
+        (
+            createVector(0, 0),
+            createVector(0, 0),
+            this.buttonColor,
+            5,
+            (button) => this.startButton.size = p5.Vector.mult(this.buttonSize, 1.1),
+            (button) => this.resize(),
+            (button) => this.startButton.color = color(.7, 1, .4),
+            (button) => this.startButton.color = this.buttonColor
+        );
 
+        window.onresize += this.resize;
+        this.resize();
     }
 
     update()
     {
-        print(Input.mouse.position);
+        this.startButton.update();
     }
 
     draw()
     {
+        this.startButton.draw();
+    }
 
+    resize()
+    {
+        this.startButton.position = createVector(width * .5, height * .5);
+        this.startButton.size = this.buttonSize;
     }
 }
