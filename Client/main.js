bd79942459b541b8a431627ce2e966fe9394f2ee
Oliver Engbjerg Christensen <oliverec04@gmail.com>
2020-01-26 15:35:17 +0100

    oliver button

--- Client/main.js
+++ Client/main.js
@@ -7,16 +7,47 @@ function setup()
     colorMode(HSB, 1);
 
     resize();
+
+    button1 = new Button
+    (
+        createVector(200, 100), 
+        createVector(269, 89), 
+        color(.25, .5, 1), 
+        "Create", 
+        color(.13, 1, 1),
+        (button) => button.size = p5.Vector.mult(button.size, 1.5), 
+        (button) => button.size = createVector(269, 89), 
+        (button) => print("onPress"), 
+        (button) => print("onRelease")
+    );
+
+    button2 = new Button
+    (
+        createVector(400, 700), 
+        createVector(269, 500), 
+        color(.25, .5, 1), 
+        "Options", 
+        0,
+        (button) => button.size = p5.Vector.mult(button.size, 1.5), 
+        (button) => button.size = createVector(269, 500), 
+        (button) => print("onPress"), 
+        (button) => print("onRelease")
+    );
 }
 
 function draw()
 {
+    button1.update();
+    button2.update();
 
+    background(0);
+
+    button1.draw();
+    button2.draw();
 }
 
-
 window.onresize += resize;
-function reszie()
+function resize()
 {
     resizeCanvas(document.documentElement.clientWidth, document.documentElement.clientHeight);
 }
--- Client/oliverButton.js
+++ Client/oliverButton.js
@@ -1,43 +1,95 @@
-///<reference path="p5.global-mode.d.ts" />
-button1 = new Button(createVector(100, 100), createVector(269, 89), "test hello world", color(169, 50, 100), print("hover test"), print("click test"));
 class Button
 {
-    constructor(position, size, text, color, onHover, onClick, textColor)
+    constructor(position, size, color, text, textColor, onEnter, onLeave, onPress, onRelease)
     {
-        this.position = position
-        this.size = size
-        this.text = text
-        this.textcolor = (0, 0, 100)
-        this.textSize = this.sizeY / 4
-        this.color = color
-        this.onClick = onClick
+        this.position = position;
+        this.size = size;
+        this.color = color;
+        this.text = text;
+        this.textColor = textColor;
+        this.textSize = this.size.y / 4;
+        this.onEnter = onEnter;
+        this.onLeave = onLeave;
+        this.onPress = onPress;
+        this.onRelease = onRelease;
     }
 
     update()
     {
-        if (this.color[2] >= 50)
-        {
-            this.textcolor = [0, 0, 0]
-        }
-    }
+        // Set min & max
+        this.min = p5.Vector.sub(this.position, p5.Vector.mult(this.size, .5));
+        this.max = p5.Vector.add(this.position, p5.Vector.mult(this.size, .5));
 
-    onClickF()
-    {
-        if (mouseX >= this.position.x && mouseX <= this.position.x + this.size.x && mouseY >= this.position.y && mouseY <= this.position.y + this.size.y)
+        // Auto text color
+        if(this.textColor == 0 && this.color.v3 > .5)
         {
-            this.onClick();
+            this.textColor = color(0, 0, 0);
+        }
+        else if(this.textColor == 0)
+        {
+            this.textColor = color(0, 0, 1);
+        }
+
+        // Text size
+        if(textWidth(this.text) > this.size.x)
+        {
+            this.textSize = this.textSize / 4;
+        }
+
+        // Detect hover
+        if(mouseX < this.max.x && mouseX > this.min.x && mouseY < this.max.y && mouseY > this.min.y)
+        {
+            if(this.isHover == false)
+            {
+                this.onEnter(this);
+            }
+
+            this.isHover = true;
+
+            // Detect press
+            if(mouseIsPressed)
+            {
+                if(this.isPressed == false)
+                {
+                    this.onPress(this);
+                }
+
+                this.isPressed = true;
+            }
+
+            // Detect release
+            else
+            {
+                if(this.isPressed == true)
+                {
+                    this.onRelease(this);
+                }
+
+                this.isPressed = false;
+            }
+        }
+
+        else
+        {
+            // Detect leave
+            if(this.isHover == true)
+            {
+                this.onLeave(this);
+            }
+
+            this.isHover = false;
         }
     }
 
     draw()
     {
-        colorMode(HSB);
-
-        fill(this.color)
-        rect(this.posX, this.posY, this.sizeX, this.sizeY)
+        fill(this.color);
+        rect(this.position.x, this.position.y, this.size.x, this.size.y);
         
-        fill(this.textcolor)
-        textSize(this.textSize)
-        text(this.text, this.posX + this.sizeX / 2 - textWidth(this.text) / 2, this.posY + this.sizeY / 2 - this.textSize / 2, this.sizeX, this.sizeY)
+        textSize(this.size.y / 2);
+        textAlign(CENTER, CENTER);
+        fill(this.textColor);
+        text(this.text, this.position.x, this.position.y);
+        text()
     }
 }
