48818b56a9e34d785a8fd72202f25a0911f429d8
Oliver Engbjerg Christensen <oliverec04@gmail.com>
2020-02-11 18:48:35 +0100

    player list

--- Client/Lobby/guest.js
+++ Client/Lobby/guest.js
@@ -2,14 +2,34 @@ class Guest
 {
     constructor()
     {
-        this.playerText = []
+        this.playerList = [];
+        this.textSize = 40;
 
-        for(var i = room.players.length; i < room.players.length; i++)
+        this.playerTitle = new Text("Players in room", createVector(this.textSize * 2, this.textSize * 2), this.textSize, color(0, .6, 1), LEFT, CENTER);
+        
+        var playerText = (name) => new Text(name, createVector(this.textSize * 2, this.playerList.length * this.textSize * 2 + this.textSize * 2 + 80), this.textSize, color(0, 0, 1), LEFT, CENTER);
+
+        for(var i = 0; i < room.players.length; i++)
         {
-            this.playerText.push(new Text(room.players[i].name, createVector(20, i * 20 + 20), 10, color(1, 1, 1), LEFT, CENTER));
+            this.playerList.push(playerText(room.players[i].name));
         }
 
+        socket.on("PlayerJoined", (player) => 
+        {
+            room.players.push(player);
+            this.playerList.push(playerText(player.name));
+        });
 
+        socket.on("PlayerLeft", (id) =>
+        {
+            var index = room.players.findIndex((player) => player.id === id);
+            room.players.splice(index, 1);
+            this.playerList.splice(index, 1);
+            for(i = index; i < this.playerList.length; i++)
+            {
+                this.playerList[i].position.y -= this.textSize * 2;
+            }
+        });
     }
 
     onResize()
@@ -24,9 +44,11 @@ class Guest
 
     draw()
     {
-        for(var i = this.playerText.length; i < this.playerText.length; i++)
+        this.playerTitle.draw();
+
+        for(var i = 0; i < this.playerList.length; i++)
         {
-            this.playerText[i].draw();
+            this.playerList[i].draw();
         }
     }
 }
