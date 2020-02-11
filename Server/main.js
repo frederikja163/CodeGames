f213f2fae204cb75af575e3883e72e8864ac6d62
Frederik Juhl Andreasen <fjaplv64@gmail.com>
2020-02-11 17:45:25 +0100

    more stuff

--- Server/main.js
+++ Server/main.js
@@ -18,7 +18,7 @@ io.on("connection", (socket) =>
 			{
 				if (rooms[i].id === id)
 				{
-					var player = new Player(socket.id, "player" + rooms[i].players.length + 1)
+					var player = new Player(socket.id, "player" + (rooms[i].players.length + 1))
 					for(var j = 0; j < rooms[i].players.length; j++)
 					{
 						io.to(rooms[i].players[j].id).emit("PlayerJoined", player);
@@ -46,7 +46,7 @@ io.on("connection", (socket) =>
 					rooms[i].players.splice(j, 1);
 					for (var k = 0; k < rooms[i].players.length; k++)
 					{
-						io.to(rooms[i].players[j].id).emit("PlayerLeft", socket.id);
+						io.to(rooms[i].players[k].id).emit("PlayerLeft", socket.id);
 					}
 					return;
 				}
