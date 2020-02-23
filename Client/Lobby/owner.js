5f1c01396391370365970581c37058211dcfe63d
Oliver Engbjerg Christensen <oliverec04@gmail.com>
2020-02-23 01:00:08 +0100

    lort owner

--- Client/Lobby/owner.js
+++ Client/Lobby/owner.js
@@ -29,9 +29,14 @@ class Owner extends Guest
             var playerNamePos = createVector(this.txtSize * 2, i * this.txtSize * 2 + this.txtSize * 4);
             var playerNameSize = createVector(textWidth(room.players[i].name), this.txtSize);
             
+            fill(.8, 1, 1);
+            rect(playerNamePos.x, playerNamePos.y, playerNameSize.x, playerNameSize.y);
+
             if (isMouseWithin(playerNamePos.x, playerNamePos.y - this.txtSize, playerNameSize.x, playerNameSize.y))
             {
                 this.color = .6;
+                this.txtSize = this.txtSize * 1.1;
+                playerNamePos.x -= 1;
 
                 if (Input.mouse.button[LEFT])
                 {
@@ -41,13 +46,14 @@ class Owner extends Guest
 
             fill(color(this.color));
             textSize(this.txtSize);
-            text(room.players[i].name, playerNamePos.x, playerNamePos.y);
+            textAlign(LEFT, CENTER);
+            text(room.players[i].name, playerNamePos.x, playerNamePos.y + playerNameSize.y * .5);
 
             if (isMouseWithin(playerNamePos.x, playerNamePos.y - this.txtSize, playerNameSize.x, playerNameSize.y))
             {
                 strokeWeight(4);
                 stroke(1, .8, 1);
-                line(playerNamePos.x - 5, playerNamePos.y - this.txtSize * .4, playerNamePos.x + playerNameSize.x + 5, playerNamePos.y - this.txtSize * .4);
+                line(playerNamePos.x - this.txtSize * .1, playerNamePos.y - this.txtSize * .1, playerNamePos.x + playerNameSize.x + this.txtSize * .1, playerNamePos.y - this.txtSize * .1);
                 strokeWeight(0);
             }
         }
