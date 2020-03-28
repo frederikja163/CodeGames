87a81b545cd2a18d548f2afab81f2be15a4d6e06
Frederik Juhl Andreasen <fjaplv64@gmail.com>
2020-03-28 17:19:43 +0100

    update to 2d array

--- Server/game.js
+++ Server/game.js
@@ -141,6 +141,7 @@ class Game
                 do
                 {
                     pos = Random.randomPosition(width, height);
+                    console.warn(pos.x + " : " + pos.y);
                 } while(board[pos.x][pos.y] != "white");
                 board[pos.x][pos.y] = type;
             }
