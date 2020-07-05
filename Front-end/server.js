9592897c8f377e359b55f1778e803dc4fa9dcbcd
OliverEC <oliverec04@gmail.com>
2020-07-05 22:33:59 +0200

    join

--- Front-end/server.js
+++ Front-end/server.js
@@ -21,11 +21,3 @@ class Server
         this.onRoomJoined = (room, rid) => {};
     }
 }
-
-const server = new Server("http://localhost", 9999);
-
-server.joinRoom();
-server.onRoomJoined = (room, rid) => {
-    server.room = room;
-    server.rid = rid;
-};
