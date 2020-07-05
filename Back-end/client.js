8306baa158c9a0c176f11b2b156d5321932630be
Frederik Juhl Andreasen <fjaplv64@gmail.com>
2020-07-05 22:16:57 +0200

    other players

--- Back-end/client.js
+++ Back-end/client.js
@@ -3,18 +3,22 @@ class Client{
     {
         let send = (message, arg1, arg2) =>{
             socket.emit(message, arg1, arg2);
-            console.log(message);
         };
-        socket.on("joinRoom", (rid) => this.onJoinRoom(this, rid))
+        socket.on("joinRoom", (rid) => this.onJoinRoom(rid));
+        socket.on("disconnect", () => this.onDisconnected());
 
         //======[Server protocol]======
         this.pid = socket.id;
+        this.room = null;
 
         //Incoming
         this.onJoinRoom = (client, rid) => {};
+        this.onDisconnected = (client) => {};
 
         //Outgoing
         this.roomJoined = (room, rid) => send("roomJoined", room, rid);
+        this.playerJoined = (room, player) => send("playerJoined", room, player);
+        this.playerLeft = (room, player) => send("playerLeft", room, player);
     }
 }
 
--- Front-end/server.js
+++ Front-end/server.js
@@ -9,16 +9,19 @@ class Server
             socket.emit(message, arg1, arg2);
         };
         socket.on("roomJoined", (room, rid) => this.onRoomJoined(room, rid));
+        socket.on("playerJoined", (room, player) => this.onPlayerJoined(room, player));
+        socket.on("playerLeft", (room, player) => this.onPlayerLeft(room, player));
 
         //======[Server protocol]======
         this.rid = null;
-        this.room = null;
 
         //Outgoing
         this.joinRoom = (rid) => send("joinRoom", rid);
         
         //Incoming
         this.onRoomJoined = (room, rid) => {};
+        this.onPlayerJoined = (room, player) => {};
+        this.onPlayerLeft = (room, player) => {};
     }
 }
 
