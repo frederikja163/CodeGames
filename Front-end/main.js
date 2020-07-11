4a55c3a2b18604ae8635c07ff15ac016da576fef
OliverEC <oliverec04@gmail.com>
2020-07-11 16:44:44 +0200

    pages

--- Front-end/main.js
+++ Front-end/main.js
@@ -1,23 +1,34 @@
-const server = new Server("http://localhost", 9999);
+const SERVER = new Server("http://localhost", 9999);
 
-server.onRoomJoined = (room, rid) => {
-    server.room = room;
-    server.rid = rid;
+const HIDDEN = "none";
+const VISIBLE = "grid";
+
+let welcome;
+let lobby;
+
+window.onload = () => {
+    welcome = document.querySelector("#welcome");
+    lobby = document.querySelector("#lobby"); 
+}
+
+SERVER.onRoomJoined = (room, rid) => {
+    SERVER.room = room;
+    SERVER.rid = rid;
     // TODO: update link, change iframe src to lobby html
 };
 
-server.onPlayerJoined = (room, player) => {
-    server.room = room;
+SERVER.onPlayerJoined = (room, player) => {
+    SERVER.room = room;
     console.log(player.pid, player.name, "joined");
 }
 
-server.onPlayerLeft = (room, player) => {
-    server.room = room;
+SERVER.onPlayerLeft = (room, player) => {
+    SERVER.room = room;
     console.log(player.pid, player.name, "left");
 }
 
-server.onNameChanged = (room, player, name) => {
-    server.room = room;
+SERVER.onNameChanged = (room, player, name) => {
+    SERVER.room = room;
     console.log(player.pid, name, "changed");
 }
 
@@ -26,6 +37,9 @@ function joinBtnOnClick()
     let name = document.getElementById("welcomeInputName").value;
     let rid = document.getElementById("welcomeInputRid").value;
     
-    server.joinRoom(rid);
-    server.setName(name);
+    SERVER.joinRoom(rid);
+    SERVER.setName(name);
+
+    welcome.style.display = HIDDEN;
+    lobby.style.display = VISIBLE;
 }
