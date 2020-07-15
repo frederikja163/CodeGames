dc77e02175f17c9a5f61dc487249b2dd8df7be05
OliverEC <oliverec04@gmail.com>
2020-07-16 01:23:19 +0200

    Player list buttons not working, and teams

--- Front-end/main.js
+++ Front-end/main.js
@@ -6,6 +6,7 @@ const VISIBLE = "grid";
 let welcome;
 let lobby;
 let debugmode = false;
+let teamNames = ["Red", "Blue", "Green", "Yellow", "Pink"];
 
 document.addEventListener('keyup', (event) => {
     if(event.keyCode == 113){
@@ -26,6 +27,10 @@ window.onload = () => {
         let rid = url.substring(ridStart + 1);
         SERVER.joinRoom(rid);
     }
+
+
+
+    console.log(SERVER);
 }
 
 SERVER.onRoomJoined = (room, rid, pid) => {
@@ -81,13 +86,52 @@ function updatePlayerList()
     document.querySelector("#lobby #players ul").innerHTML = "";
 
     let playerList = document.querySelector("#lobby #players ul");
-    let liList = [];
 
     for (let i = 0; i < SERVER.room.players.length; i++)
     {
-        liList[i] = document.createElement("LI");
-        liList[i].innerHTML = SERVER.room.players[i].name + "\t" + (debugmode ? SERVER.room.players[i].pid : "");
-        playerList.appendChild(liList[i]);
+        // Create list item
+        let li = document.createElement("LI");
+
+        // Create name div
+        let divName = document.createElement("DIV");
+        divName.innerText = SERVER.room.players[i].name + "\t" + (debugmode ? SERVER.room.players[i].pid : "");
+        li.appendChild(divName);
+
+        // Create button div
+        let divBtn = document.createElement("DIV");
+
+        // Create spymaster button
+        let btnSm = document.createElement("BUTTON");
+        btnSm.className = "btn2";
+        btnSm.innerText = "S";
+        divBtn.appendChild(btnSm);
+
+        // Create kick button
+        let btnKick = document.createElement("BUTTON");
+        btnKick.className = "btn2";
+        btnKick.innerText = "K";
+        btnKick.setAttribute('onclick', 'SERVER.kickPlayer("' + SERVER.room.players[i].pid + '", \"Fuck off\")');
+        divBtn.appendChild(btnKick);
+
+        // Create move up team button
+        let btnUp = document.createElement("BUTTON");      
+        btnUp.className = "btn2";
+        btnUp.innerText = "^";
+        btnUp.setAttribute("onclick", "SERVER.setTeam('" + SERVER.room.players[i].pid + "', '" + String(SERVER.room.players[i].team - 1) + "')");
+        divBtn.appendChild(btnUp);
+
+        // Create move down team button
+        let btnDown = document.createElement("BUTTON");
+        btnDown.className = "btn2";
+        btnDown.innerText = "V";
+        btnDown.setAttribute("onclick", "SERVER.setTeam('" + SERVER.room.players[i].pid + "', '" + String(SERVER.room.players[i].team + 1) + "')");
+        divBtn.appendChild(btnDown);
+
+        // Add button div to list item
+        li.appendChild(divBtn);
+
+        // Add list item to player list
+        playerList.appendChild(li);
     }
 }
 
@@ -95,10 +139,26 @@ function updatePlayerList()
 
 function addTeam()
 {
+    // Create team box
+    let team = document.createElement("DIV");
+    team.className = "box";
 
+    // Create team title
+    let title = document.createElement("H3");
+    title.innerHTML = "Team";
+    team.appendChild(title);
+
+    // Create team list
+    let list = document.createElement("UL");
+    team.appendChild(list);
+
+    // Add team to player list
+    document.querySelector("#lobby #players").appendChild(team);
+
+    updatePlayerList();
 }
 
 function removeTeam()
 {
-
+    document.querySelector("#lobby #players").removeChild(document.querySelector("#lobby #players").lastChild);
 }
