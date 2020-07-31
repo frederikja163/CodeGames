9cfa985a22d11f8ab9b37de1f7b7b316790f701d
Frederik Juhl Andreasen <fjaplv64@gmail.com>
2020-08-01 00:45:57 +0200

    command module

--- Back-end/main.js
+++ Back-end/main.js
@@ -1,8 +1,11 @@
 var io = require("socket.io").listen(9999);
 var {Room} = require("./room.js");
 var {Client} = require("./client.js");
+var Command = require("./commands.js");
 
 io.on("connection", socket =>
 {
     Room.OnConnected(new Client(socket));
 });
+
+Command.initialize();
