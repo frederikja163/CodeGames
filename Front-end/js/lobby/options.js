7c57ac1ab9250d8bc0b62554cbadc31c9811b612
Frederik Juhl Andreasen <fjaplv64@gmail.com>
2020-09-17 22:14:37 +0200

    begin work on packs

--- Front-end/js/lobby/options.js
+++ Front-end/js/lobby/options.js
@@ -1,3 +1,5 @@
+const LANGS = ["EN"]
+
 function updateTeamCount()
 {
     let teamsOptElement = document.querySelector("#lobby #options > ul > li:nth-child(1)");
@@ -9,4 +11,22 @@ function teamCountChanged()
     // Change team count in options
     let teamsOptElement = document.querySelector("#lobby #options ul li:nth-child(1)");
     teamsOptElement.innerText = "Team count: " + String(SERVER.room.options.teamCount);
+}
+
+function initializePackList()
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", 'https://frederikja163.github.io/CodeGames/Front-end/index.html', true);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
 }
