931c0647994e5d7fa89285c998366768ad74bcd8
Frederik Juhl Andreasen <fjaplv64@gmail.com>
2020-09-21 20:51:42 +0200

    pack data loading

--- Front-end/js/lobby/options.js
+++ Front-end/js/lobby/options.js
@@ -1,6 +1,7 @@
 const HTTP = new XMLHttpRequest();
 const BASEURL = "http://codegames.ga/";
 const PACKURL = BASEURL + "Front-end/assets/packs/";
+let languages = [];
 
 function updateTeamCount()
 {
@@ -17,10 +18,28 @@ function teamCountChanged()
 
 function initializePackList()
 {
-    HTTP.open("GET", PACKURL + "lang.csv");
-    HTTP.send();
-    console.log(HTTP.responseText);
-    HTTP.open("GET", PACKURL + "pack.csv");
-    HTTP.send();
-    console.log(HTTP.responseText);
+    fetch(PACKURL + "lang.csv")
+        .then(r => r.text())
+        .then(t => t.split(',').forEach(lang => languages.push(createLang(lang.trim()))));
+    // HTTP.open("GET", PACKURL + "lang.csv");
+    // HTTP.send();
+    // console.log(HTTP.responseText);
+    // HTTP.open("GET", PACKURL + "packs.csv");
+    // HTTP.send();
+    // console.log(HTTP.responseText);
+}
+
+function createLang(lang)
+{
+    let l = {};
+    let i = lang.indexOf('[');
+    let code = lang.substring(i + 1, lang.length - 1);
+    l.code = code;
+    let name = lang.substring(0, i-1);
+    l.name = name;
+    l.packs = [];
+    fetch(PACKURL + code.toLowerCase() + "/packs.csv")
+        .then(r => r.text())
+        .then(t => t.split(',').forEach(p => l.packs.push(p)));
+    console.log(l);
 }
