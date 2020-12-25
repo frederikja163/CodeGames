d3385a9c8bb6f2ea3b54db290ad93850371fb9c4
FrederikJA <fjaplv64@gmail.com>
2020-12-25 19:53:27 +0100

    include data.js

---
+++ Back-end/data.js
@@ -0,0 +1,48 @@
+class RoomData
+{
+    constructor(rid)
+    {
+        this.rid = rid;
+        this.players = [];
+        this.options = new Options();
+        this.words = undefined;
+    }
+}
+
+class PlayerData
+{
+    constructor(pid)
+    {
+        this.pid = pid;
+        this.spymaster = false;
+        this.name = "Player";
+        this.team = -1;
+    }
+}
+
+class Options
+{
+    constructor()
+    {
+        this.words = [];
+        this.teamCount = 2;
+    }
+}
+
+class Word
+{
+    constructor(str, team)
+    {
+        this.word = str;
+        this.team = team;
+    }
+}
+
+
+module.exports =
+{
+    RoomData,
+    PlayerData,
+    Options,
+    Word
+}
