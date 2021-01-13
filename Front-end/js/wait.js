8923c90515ff4152ee19e33ca9d31c37d1992812
OliverEC <oliverec04@gmail.com>
2021-01-13 21:23:51 +0100

    add files

---
+++ Front-end/js/wait.js
@@ -0,0 +1,8 @@
+function swapToWait()
+{
+    welcome.style.display = "none";
+    lobby.style.display = "none";
+    game.style.display = "none";
+    wait.style.display = "grid";
+    state = "wait";
+}
